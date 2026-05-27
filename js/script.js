48db8cb274ebcce1edd1b009433ebf7259fbbb56
jhohan11 <jhohan11gomher@gmail.com>
2026-05-27 17:57:59 -0600

    refactor: Se agrega movimiento automatico al carrusel. (style,js)

--- js/script.js
+++ js/script.js
@@ -18,23 +18,45 @@ if (form) {
 }
 
 const slides = document.querySelectorAll(".carousel-slide");
+const carousel = document.querySelector(".carousel");
 const prevBtn = document.querySelector(".carousel-prev");
 const nextBtn = document.querySelector(".carousel-next");
 let currentSlide = 0;
+let carouselTimer;
 
 function showSlide(index) {
   slides.forEach((slide) => slide.classList.remove("active"));
   slides[index].classList.add("active");
 }
 
-if (slides.length && prevBtn && nextBtn) {
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function startCarousel() {
+  carouselTimer = setInterval(nextSlide, 4000);
+}
+
+function resetCarouselTimer() {
+  clearInterval(carouselTimer);
+  startCarousel();
+}
+
+if (slides.length && carousel && prevBtn && nextBtn) {
   prevBtn.addEventListener("click", () => {
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     showSlide(currentSlide);
+    resetCarouselTimer();
   });
 
   nextBtn.addEventListener("click", () => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
+    nextSlide();
+    resetCarouselTimer();
   });
+
+  carousel.addEventListener("mouseenter", () => clearInterval(carouselTimer));
+  carousel.addEventListener("mouseleave", startCarousel);
+
+  startCarousel();
 }
